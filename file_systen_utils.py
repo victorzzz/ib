0a9b0353d32d4f9223bda53e9e300c1a24ef8144
Victor <viktorzhuravel@gmail.com>
2023-12-27 23:50:40 -0500

    Implement raw data merger

--- file_systen_utils.py
+++ file_systen_utils.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import datetime
 
 def is_file_exists(file_path:str) -> bool:
@@ -23,3 +24,25 @@ def iterate_files_name_only(folder_path:str):
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             yield file  
+
+def delete_file(file_path:str):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+def delete_folder(folder_path:str):
+    if os.path.exists(folder_path):
+        os.rmdir(folder_path)
+
+def move_file_to_folder(file_path:str, destination_folder:str):
+    """
+    Moves a file from file_path to the destination_folder.
+
+    :param file_path: The full path of the file to be moved.
+    :param destination_folder: The folder where the file should be moved.
+    """
+    # Check if destination folder exists, create it if it doesn't
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+
+    # Move the file
+    shutil.move(file_path, destination_folder)
--- ib_raw_data_merger.py
+++ ib_raw_data_merger.py
@@ -13,9 +13,11 @@ def merge_csv_files(tickers:List[Dict[str, Dict[str, int]]], raw_files:List[str]
     print(f"Starting rocessing '{', '.join(tickers)}' ...")
 
     for ticker in tickers:
+
         for ticker_info in ticker.items():
             ticker_symbvol:str = ticker_info[0]
             ticker_contracts:Dict[str,int] = ticker_info[1]
+            
             for contarct in ticker_contracts.items():
                 exchange:str = contarct[0]
                 contract_id:int = contarct[1]
@@ -23,7 +25,11 @@ def merge_csv_files(tickers:List[Dict[str, Dict[str, int]]], raw_files:List[str]
                 for minute_multiplier in cnts.minute_multipliers:
                     filtered_raw_files:List[str] = [file for file in raw_files if file.startswith(f"{cnts.data_folder}/{ticker_symbvol}-{contract_id}--ib--{minute_multiplier:.0f}--minute--")]
 
-                    merged_file_name = f"{cnts.merged_data_folder}/{ticker_symbvol}-{contract_id}--ib--{minute_multiplier:.0f}--minute--merged.csv"
+                    if len(filtered_raw_files) == 0:
+                        continue
+
+                    merged_file_name = f"{cnts.merged_data_folder}/{ticker_symbvol}-{contract_id}-{exchange}--ib--{minute_multiplier:.0f}--minute--merged.csv"
+
                     merged_data_frame:pd.DataFrame = pd.DataFrame()
                     if exists(merged_file_name):
                         merged_data_frame = pd.read_csv(merged_file_name, index_col=0)
@@ -35,7 +41,16 @@ def merge_csv_files(tickers:List[Dict[str, Dict[str, int]]], raw_files:List[str]
 
                         raw_data_frame = raw_data_frame.copy() # performance optimization
                         merged_data_frame = pd.concat([merged_data_frame, raw_data_frame], axis=0)
+                    
+                    merged_data_frame.sort_values(by='timestamp', inplace=True)
+                    merged_data_frame.drop_duplicates(inplace=True)
 
+                    print(f"Saving {merged_file_name} ...")
+                    merged_data_frame.to_csv(merged_file_name, index=False)
+
+                    print(f"Archiving {', '.join(filtered_raw_files)} ...")
+                    for raw_file in filtered_raw_files:
+                        fsu.move_file_to_folder(raw_file, cnts.data_archived_folder)
 
 def do_step():
     processes = []
